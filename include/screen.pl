10cbec87fcc8412bbd5f4a7494a1263b3ae69318
Daniel Friesel <derf@derf.homelinux.org>
2009-11-07 18:50:28 +0100

    screen.pl: Use unicode

--- include/screen.pl
+++ include/screen.pl
@@ -206,7 +206,7 @@ sub print_battery {
 	given($info{charging_state}) {
 		when('discharging') {
 			$buf .= sprintf(
-				' v %.f%%, %02d:%02.fh remaining',
+				' ↓ %.f%%, %02d:%02.fh remaining',
 				$capacity,
 				$info{remaining_capacity} / $info{present_rate},
 				($info{remaining_capacity} * 60 / $info{present_rate}) % 60,
@@ -214,7 +214,7 @@ sub print_battery {
 		}
 		when('charging') {
 			$buf .= sprintf(
-				' ^ %.f%%, %02d:%02.fh remaining',
+				' ↑ %.f%%, %02d:%02.fh remaining',
 				$capacity,
 				($info{last_full_capacity} - $info{remaining_capacity}) / $info{present_rate},
 				(($info{last_full_capacity} - $info{remaining_capacity}) * 60 / $info{present_rate}) % 60,
@@ -222,7 +222,7 @@ sub print_battery {
 		}
 		when('full') {
 			$buf .= sprintf(
-				' = %.f%%, %.f%% health',
+				' → %.f%%, %.f%% health',
 				$capacity,
 				$health,
 			);
