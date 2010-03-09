2c18a65d64fa232e79f7036d8cd1b9e516086f55
Daniel Friesel <derf@derf.homelinux.org>
2010-03-09 19:50:40 +0100

    Remove , in battery part

--- include/screen.pl
+++ include/screen.pl
@@ -264,7 +264,7 @@ sub print_battery {
 	given($info{charging_state}) {
 		when('discharging') {
 			$buf .= sprintf(
-				' ↓ %.f%%, %02d:%02.fh remaining',
+				' ↓ %.f%%  %02d:%02.fh remaining',
 				$capacity,
 				$info{remaining_capacity} / $info{present_rate},
 				($info{remaining_capacity} * 60 / $info{present_rate}) % 60,
@@ -272,7 +272,7 @@ sub print_battery {
 		}
 		when('charging') {
 			$buf .= sprintf(
-				' ↑ %.f%%, %02d:%02.fh remaining',
+				' ↑ %.f%%  %02d:%02.fh remaining',
 				$capacity,
 				($info{last_full_capacity} - $info{remaining_capacity}) / $info{present_rate},
 				(($info{last_full_capacity} - $info{remaining_capacity}) * 60 / $info{present_rate}) % 60,
@@ -280,7 +280,7 @@ sub print_battery {
 		}
 		when('full') {
 			$buf .= sprintf(
-				' → %.f%%, %.f%% health',
+				' → %.f%%  %.f%% health',
 				$capacity,
 				$health,
 			);
